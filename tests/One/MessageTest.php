7a63eeb2b72f4ee0f9bf7f422ce7d1c0b52d1d8f
Mior Muhammad Zaki <crynobone@gmail.com>
2018-03-12 10:17:23 +0800

    Improves tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/One/MessageTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Silverstreet\TestCase\One;
+
+use Mockery as m;
+use Silverstreet\Client;
+use Silverstreet\One\Message;
+use PHPUnit\Framework\TestCase;
+use Laravie\Codex\Testing\FakeRequest;
+
+class MessageTest extends TestCase
+{
+    /**
+     * Teardown the test environment.
+     */
+    protected function tearDown()
+    {
+        m::close();
+    }
+
+    /** @test */
+    public function it_can_send_message()
+    {
+        $body = [
+            'username' => 'foo',
+            'password' => 'bar',
+        ];
+
+        $faker = FakeRequest::create()
+                    ->expectEndpointIs('https://api.silverstreet.com/send.php')
+                    ->call('POST', [], m::type('GuzzleHttp\Psr7\Stream'))
+                    ->shouldResponseWith(200, '1');
+
+        $client = new Client($faker->http(), $body['username'], $body['password']);
+
+        $response = $client->uses('Message')->text('Hello world', '+60123456789', 'KATSANA');
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame('1', $response->getBody());
+    }
+}
