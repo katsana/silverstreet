321ea864f45dad02f772434f1d3bc68ea81a5b1f
Mior Muhammad Zaki <crynobone@gmail.com>
2018-12-13 18:19:13 +0800

    Backport credit balance check for 2.1 release.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/One/CreditTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Silverstreet\TestCase\One;
+
+use Laravie\Codex\Testing\Faker;
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use Silverstreet\Client;
+
+class BalanceTest extends TestCase
+{
+    /**
+     * Teardown the test environment.
+     */
+    protected function tearDown(): void
+    {
+        m::close();
+    }
+
+    /** @test */
+    public function it_can_check_available_balance()
+    {
+        $body = [
+            'username' => 'foo',
+            'password' => 'bar',
+        ];
+
+        $faker = Faker::create()
+                    ->send('POST', [], 'username=foo&password=bar')
+                    ->expectEndpointIs('https://api.silverstreet.com/creditcheck.php')
+                    ->shouldResponseWith(200, '<?xml version="1.0" encoding="ISO-8859-1"?>
+<?xml-stylesheet type="text/xsl" href="creditcheck.xsl"?>
+<credits>
+    <balance>1437</balance>
+</credits>');
+
+        $client = new Client($faker->http(), $body['username'], $body['password']);
+
+        $response = $client->uses('Credit')->available();
+
+        $this->assertSame(1437, $response);
+    }
+}
