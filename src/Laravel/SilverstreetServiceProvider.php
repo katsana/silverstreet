117da2cfb7a0d9105153a6d3baeca5e474c52de5
Mior Muhammad Zaki <crynobone@gmail.com>
2019-04-09 20:31:44 +0800

    Add laravel service provider.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Laravel/SilverstreetServiceProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Silverstreet\Laravel;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+use Laravie\Codex\Discovery;
+use Silverstreet\Client;
+
+class SilverstreetServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    /**
+     * Register the service.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('silverstreet', function (Application $app) {
+            return $this->createSilverstreetClient(
+                $app->make('config')->get('services.silverstreet')
+            );
+        });
+    }
+
+    /**
+     * Create Silverstreet Client.
+     *
+     * @param array $config
+     *
+     * @return \Silverstreet\Client
+     */
+    protected function createSilverstreetClient(array $config): Client
+    {
+        return new Client(
+            $this->createHttpClient(), $config['username'], $config['password']
+        );
+    }
+
+    /**
+     * Create HTTP Client.
+     *
+     * @return \Http\Client\Common\HttpMethodsClient
+     */
+    protected function createHttpClient()
+    {
+        return Discovery::client();
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['silverstreet'];
+    }
+}
